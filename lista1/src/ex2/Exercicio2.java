cef9ca9a294fc8c720799697f684bfc13bd36e9c
luís sabel <luis.g2005@aluno.ifsc.edu.br>
2023-03-07 08:49:18 -0300

    Lista1

---
+++ lista1/src/ex2/Exercicio2.java
@@ -0,0 +1,20 @@
+package ex2;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> elementos = new ArrayList();
+
+		for (int i = 0; i < 10; i++) {
+			String qnt = JOptionPane.showInputDialog("escreva o elemento:");
+
+			elementos.add(qnt);
+		}
+		JOptionPane.showMessageDialog(null, elementos);
+	}
+
+}
